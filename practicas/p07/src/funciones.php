68264e31ca8f6e297f83b780fa663f3683fd6f78
Ivan Luna <crash2514@outlook.com>
2025-09-27 21:56:01 -0600

    Corrección para un mejor funcionamiento

--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -2,13 +2,8 @@
 // funciones.php
 
 // 1. Verificar si un número es múltiplo de 5 y 7
-
 function esMultiploDe5y7($numero) {
-    if ($numero % 5 == 0 && $numero % 7 == 0) {
-        return "El número $numero SÍ es múltiplo de 5 y 7";
-    } else {
-        return "El número $numero NO es múltiplo de 5 y 7";
-    }
+    return ($numero % 5 == 0 && $numero % 7 == 0);
 }
 
 // 2. Generar números hasta secuencia impar-par-impar
