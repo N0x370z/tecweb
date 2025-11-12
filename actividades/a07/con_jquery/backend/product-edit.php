70b294a75316b4f6fbdeead60303f46a0899dfdd
Ivan Luna <crash2514@outlook.com>
2025-11-11 23:41:04 -0600

    Archivos modificados para el funcionamiento adecuado de la actividad

--- actividades/a07/con_jquery/backend/product-edit.php
+++ actividades/a07/con_jquery/backend/product-edit.php
@@ -1,51 +1,16 @@
 <?php
-    include_once __DIR__.'/database.php';
+use TECWEB\MYAPI\Products;
+require_once __DIR__ . '/myapi/Products.php';
 
-    // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
-    $producto = file_get_contents('php://input');
-    $data = array(
-        'status'  => 'error',
-        'message' => 'Error al actualizar el producto'
-    );
-    
-    if(!empty($producto)) {
-        // SE TRANSFORMA EL STRING DEL JSON A OBJETO
-        $jsonOBJ = json_decode($producto);
-        
-        // SE VERIFICA QUE VENGA EL ID
-        if(isset($jsonOBJ->id)) {
-            // Verificar si ya existe otro producto con el mismo nombre (excepto el actual)
-            $sql = "SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND id != {$jsonOBJ->id} AND eliminado = 0";
-            $result = $conexion->query($sql);
-            
-            if($result->num_rows == 0) {
-                $conexion->set_charset("utf8");
-                $sql = "UPDATE productos SET 
-                        nombre = '{$jsonOBJ->nombre}',
-                        marca = '{$jsonOBJ->marca}',
-                        modelo = '{$jsonOBJ->modelo}',
-                        precio = {$jsonOBJ->precio},
-                        detalles = '{$jsonOBJ->detalles}',
-                        unidades = {$jsonOBJ->unidades},
-                        imagen = '{$jsonOBJ->imagen}'
-                        WHERE id = {$jsonOBJ->id}";
-                
-                if($conexion->query($sql)) {
-                    $data['status'] = "success";
-                    $data['message'] = "Producto actualizado correctamente";
-                } else {
-                    $data['message'] = "ERROR: No se ejecutó $sql. " . mysqli_error($conexion);
-                }
-            } else {
-                $data['message'] = "Ya existe un producto con ese nombre";
-            }
-            
-            $result->free();
-        }
-        
-        $conexion->close();
+$productos = new Products('marketzone');
+$producto = file_get_contents('php://input');
+
+if (!empty($producto)) {
+    $jsonOBJ = json_decode($producto);
+    if (isset($jsonOBJ->id)) {
+        $productos->edit($jsonOBJ);
     }
+}
 
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+echo $productos->getData();
 ?>
--- actividades/a07/con_jquery/backend/product-single.php
+++ actividades/a07/con_jquery/backend/product-single.php
@@ -1,33 +1,13 @@
 <?php
-    include_once __DIR__.'/database.php';
+use TECWEB\MYAPI\Products;
+require_once __DIR__ . '/myapi/Products.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array();
-    
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if(isset($_GET['id'])) {
-        $id = $_GET['id'];
-        
-        // SE REALIZA LA QUERY DE BÚSQUEDA
-        $sql = "SELECT * FROM productos WHERE id = {$id}";
-        
-        if($result = $conexion->query($sql)) {
-            // SE OBTIENE EL RESULTADO
-            $row = $result->fetch_assoc();
-            
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
-                }
-            }
-            $result->free();
-        } else {
-            die('Query Error: '.mysqli_error($conexion));
-        }
-        $conexion->close();
-    }
-    
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+$productos = new Products('marketzone');
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $productos->single($id);
+}
+
+echo $productos->getData();
 ?>
